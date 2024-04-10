817094482b29f6bc609f6a5e610117f636080c03
Alex B <alex.8ulavin@gmail.com>
2024-04-10 20:43:09 +0300

    add notebooks models

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,2 +1,16 @@
+import java.util.*;
 public class main {
+    public static void main(String[] args) {
+
+        Set<notebook> set = new HashSet<>();
+        set.add(new notebook("Notebook 1", 8, "Windows10", 70000, "HP"));
+        set.add(new notebook("Notebook 2", 16, "Windows10", 85000, "Lenovo"));
+        set.add(new notebook("Notebook 3", 32, "linux", 85000, "Machenike"));
+        set.add(new notebook("Notebook 4", 64, "linux", 90000, "Maibenben"));
+        set.add(new notebook("Notebook 5", 16, "MacOS", 123000, "Macbook"));
+
+        operations operation = new operations(set);
+        operation.start();
+
+    }
 }
