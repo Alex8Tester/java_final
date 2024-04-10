c9b2998416147a72732ea2c16b85d33e1b0dce38
Alex B <alex.8ulavin@gmail.com>
2024-04-10 19:56:43 +0300

    add properties of notebook

--- src/main/java/notebook.java
+++ src/main/java/notebook.java
@@ -1,9 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class notebook {
     private String name;
     private int ram;
     private String os;
     private int price;
     private String model;
+
+    public notebook(String name, int ram, String os, int price, String model) {
+        this.name = name;
+        this.ram = ram;
+        this.os = os;
+        this.price = price;
+        this.model = model;
+    }
+    public boolean validateObject(){
+        return true;
+    }
+
+    public static List<String> searchProperties(){
+        List<String> list = new ArrayList<>();
+        list.add("ram");
+        list.add("os");
+        list.add("price");
+        list.add("model");
+
+        return list;
+    }
 
 
 }
